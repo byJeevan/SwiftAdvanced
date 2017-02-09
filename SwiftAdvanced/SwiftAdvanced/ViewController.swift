615edd29111d1668dcca32335653a493443d2a91
Jeevan <jeevan8082@gmail.com>
2017-02-09 14:39:06 +0530

    Project created

---
+++ SwiftAdvanced/SwiftAdvanced/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAdvanced
+//
+//  Created by Jeevan on 09/02/17.
+//  Copyright © 2017 com.iosteam.swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
