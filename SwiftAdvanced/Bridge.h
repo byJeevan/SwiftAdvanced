2a79e19950f42f44decb42dba312a0486ee0e786
kasturi gopal <kasturi.gopal@Mac-14-15-142.local>
2017-02-09 16:14:52 +0530

    Added AFNetworking Framework with Podfiles

---
+++ SwiftAdvanced/Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Bridge.h
+//  SwiftAdvanced
+//
+//  Created by kasturi gopal on 09/02/17.
+//  Copyright © 2017 com.iosteam.swift. All rights reserved.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+#import <AFNetworking/AFNetworking.h>
+#endif /* Bridge_h */
