8cf379b367a62fc05fb962533bc5d434590faa29
Jeevan <jeevan8082@gmail.com>
2017-02-15 17:37:46 +0530

    Error codes

--- SwiftAdvanced/SwiftAdvanced/BaseViewController.swift
+++ SwiftAdvanced/SwiftAdvanced/BaseViewController.swift
@@ -11,11 +11,31 @@ import UIKit
 
 
 class BaseViewController : UIViewController {
-  
+    
     func showErrorResponse(error:Error) {
-      
-        let alertview = UIAlertView.init(title: "Error Occured", message: error.localizedDescription, delegate: self, cancelButtonTitle: "OK");
+        
+        let alertview = UIAlertView.init(title: "Opps!", message: self.getMessageFromError(error:error), delegate: self, cancelButtonTitle: "OK");
         alertview .show()
+        
+    }
+    
+    func getMessageFromError(error:Error) -> String {
+        //Reference : http://nshipster.com/nserror/
+        switch (error as NSError).code  {
+            
+        case -1001:
+            return "Connection timed out"
+            
+        case -1005:
+            return "The connection failed because the network connection was lost"
+            
+        case -1009:
+            return "Device not connected to internet"
+            
+        default:
+            return "Unknown error occured"
+        }
+        
         
     }
     
